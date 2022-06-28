e266464d984955e57825a3a27cc2944e7922a7b4
AndrejGuliev <guliev.andrej@gmail.com>
2022-06-28 16:36:00 +0300

    base bot

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"telegrambot/telegram"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("5346332802:AAFdLv75U13NY8YyBMqdxcwknSbqCSWPeCI")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	tBot := telegram.NewBot(bot)
+	tBot.Start()
+
+}
---
+++ telegram/telegram.go
@@ -0,0 +1,38 @@
+package telegram
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+type Bot struct {
+	bot *tgbotapi.BotAPI
+}
+
+func NewBot(bot *tgbotapi.BotAPI) *Bot {
+	return &Bot{bot: bot}
+}
+
+func (b *Bot) Start() error {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := b.bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		b.bot.Send(msg)
+	}
+	return nil
+}
